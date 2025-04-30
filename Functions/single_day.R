c115bf9904878443d54e39b59db0edb841b0443e
Henry Traynor <henrytraynor944@gmail.com>
2025-04-30 14:13:30 -0400

    updating parms after changes to movement function

--- Functions/single_day.R
+++ Functions/single_day.R
@@ -1,10 +1,10 @@
 #Sourcing
 source("Functions/daytime_func.R")
-source("Functions/movement_func.R")
+source("Functions/movement2.R")
 
 #Completes movement of bats and intra-roost transmission in one line
-singleDay <- function(roosts, roost_dist, roost_parms, SIR_parms, num_roosts, phi_max=0.25) {
-  roosts <- movement(roosts, num_roosts=roost_parms[1], phi_max=0.25, roost_dist)
-  roosts <- daytime(roosts, roost_parms, SIR_parms)
-  return(roosts)
+singleDay <- function(roosts, roost_dist, roost_parms, SIR_parms, scores, phi_max=0.25) {
+  new_roosts <- movement2(roosts, num_roosts=roost_parms[1], phi_max=0.25, roost_dist, scores)
+  new_roosts <- daytime(new_roosts, roost_parms, SIR_parms)
+  return(new_roosts)
 }
