43dd8220b8befc9bf11c99d564458aca22b6a93a
Henry Traynor <henrytraynor944@gmail.com>
2025-04-30 14:12:42 -0400

    switch to y for clarity browser() for debugging

--- Functions/daytime_func.R
+++ Functions/daytime_func.R
@@ -1,18 +1,29 @@
-SIRleap <- function(x, SIR_parms) {
-  S <- x$S
-  I <- x$I
-  R <- x$R
-  N <- x$N
+SIRleap <- function(y, SIR_parms) {
+  S <- y$S
+  I <- y$I
+  R <- y$R
+  N <- y$N
   beta <- SIR_parms["beta"]
   gamma <- SIR_parms["gamma"]
-  
-  prob <- c(1-exp(-1*beta*S*I/N),
-            1-exp(-1*gamma))
+  if(N<=0) {
+    prob <- c(0,
+              1-exp(-1*gamma))
+  }
+  else {
+    prob <- c(1-exp(-1*beta*S*I/N),
+              1-exp(-1*gamma))
+  }
   num <- c(rbinom(1,S,prob[1]),
            rbinom(1,I,prob[2]))
   
-  x[1:3] <- x[1:3] + c(-1*num[1], num[1]-num[2], num[2])
-  return(x)
+  y[1:3] <- y[1:3] + c(-1*num[1], num[1]-num[2], num[2])
+  for(i in 1:length(y)) {
+    if(y[i] < 0 || is.na(y[i])) {
+      browser()
+      #y[i] <- 0
+    }
+  }
+  return(y)
 }
 
 #applies SIRleap() to all roosts
@@ -20,10 +31,22 @@ SIRleap <- function(x, SIR_parms) {
 daytime <- function(roosts, roost_parms, SIR_parms, model="SIR") {
   if(model=="SIR") {
     for(i in 1:roost_parms[1]) {
-      x <- roosts[i,1:4]
-      x <- SIRleap(x, SIR_parms)
-      roosts[i,1:4] <- x
+      y <- roosts[i,1:4]
+      y <- SIRleap(y, SIR_parms)
+      roosts[i,1:4] <- y
+      for(j in 1:length(y)) {
+        if(y[j]< 0 || is.na(y[j])) {
+          browser()
+        }
+      }
     }
   }
   return(roosts)
+}
+
+roost_series <- list()
+roost_series[[1]] <- roosts
+
+for(i in 2:500) {
+  roost_series[[i]] <- daytime(roost_series[[i-1]], roost_parms, SIR_parms, model="SIR")
 }
