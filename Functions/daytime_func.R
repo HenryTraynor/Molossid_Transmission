76ebc4bb8a3b1defbccc3c5a4f16669789bc38d0
Henry Traynor <henrytraynor944@gmail.com>
2025-09-10 13:46:20 -0400

    added SIRS leap

--- Functions/daytime_func.R
+++ Functions/daytime_func.R
@@ -7,7 +7,8 @@ SIRleap <- function(y, SIR_parms) {
   gamma <- SIR_parms["gamma"]
   if(N<=0) {
     prob <- c(0,
-              1-exp(-1*gamma))
+              0# 1-exp(-1*gamma)
+              )
   }
   else {
     prob <- c(1-exp(-1*beta*S*I/N),
@@ -17,6 +18,32 @@ SIRleap <- function(y, SIR_parms) {
            rbinom(1,I,prob[2]))
   
   y[1:3] <- y[1:3] + c(-1*num[1], num[1]-num[2], num[2])
+  return(y)
+}
+
+SIRSleap <- function(y, SIRS_parms) {
+  S <- y$S
+  I <- y$I
+  R <- y$R
+  N <- y$N
+  beta <- SIRS_parms["beta"]
+  gamma <- SIRS_parms["gamma"]
+  mu <- SIRS_parms["mu"]
+  
+  if(N<=0) {
+    num <- c(0,0,0)
+  }
+  else {
+    # prob <- c(infection, recovering, immunity wanes)
+    prob <- c(1-exp(-1*beta*S*I/N),
+              1-exp(-1*gamma),
+              1-exp(-1*mu))
+    num <- c(rbinom(1,S,prob[1]), # infections
+             rbinom(1,I,prob[2]), # recoveries
+             rbinom(1,R,prob[3])) # losses of immunity
+  }
+  
+  y[1:3] <- y[1:3] + c(num[3]-num[1], num[1]-num[2], num[2]-num[3])
   return(y)
 }
 
