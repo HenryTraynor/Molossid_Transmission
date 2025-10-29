c92b4827baf935350cf8ac0455ce06e51ba195fe
Henry Traynor <henrytraynor944@gmail.com>
2025-10-29 18:01:41 -0400

    safety

--- Functions/single_day.R
+++ Functions/single_day.R
@@ -1,6 +1,19 @@
 #Completes movement of bats and intra-roost transmission in one line
 singleDay <- function(roosts, roost_dist, roost_parms, inf_parms, model, scores, phi_max=0.25) {
-  new_roosts <- movement2(roosts, num_roosts=roost_parms[1], phi_max=0.25, roost_dist, scores)
-  new_roosts <- daytime(new_roosts, roost_parms, inf_parms, model="SIR")
+  # Defensive sanitation
+  roosts[is.na(roosts)] <- 0
+  roosts$N[roosts$N < 0 | is.na(roosts$N)] <- 0
+  roosts$N_max[is.na(roosts$N_max) | roosts$N_max <= 0] <- 1
+  
+  new_roosts <- movement2(
+    roosts = roosts,
+    num_roosts = roost_parms[1],
+    phi_max = phi_max,
+    roost_dist = roost_dist,
+    scores = scores
+  )
+  
+  # Daytime infection process
+  new_roosts <- daytime(new_roosts, roost_parms, inf_parms, model = "SIR")
   return(new_roosts)
 }
